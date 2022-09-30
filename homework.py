ea8d8b8b1e8649d52ed0a4f7803b461f3cf3fa9d
Владислав Бородай <vrboroday@yandex.ru>
2022-09-30 16:27:52 +0400

    Complete

--- homework.py
+++ homework.py
@@ -1,11 +1,26 @@
-...
+"""
+This program create from YP for me.
+
+Author: Boroday Vladislav
+Date: 30.09.2022
+"""
+from http import HTTPStatus
+from logging import FileHandler, Formatter, getLogger, StreamHandler
+from dotenv import load_dotenv
+
+import os
+import telegram
+import requests
+import logging
+import time
+import sys
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -18,65 +33,129 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='assistant.log',
+    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+)
+
+file = FileHandler('assistantYP.log')
+logger = getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = StreamHandler(sys.stdout)
+formatter = Formatter('%(asctime)s, %(levelname)s, %(message)s, %(name)s')
+logger.addHandler(handler)
+logger.addHandler(file)
+file.setFormatter(formatter)
+
 
 def send_message(bot, message):
-    ...
+    """Message sent from bot."""
+    try:
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID,
+                         text=message)
+        logger.info('Message sent')
+    except Exception:
+        logger.error('Error message sent')
 
 
 def get_api_answer(current_timestamp):
+    """Request to API."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-
-    ...
+    response = requests.get(url=ENDPOINT,
+                            headers=HEADERS,
+                            params=params)
+    logger.info('Request to API')
+    if response.status_code == HTTPStatus.OK:
+        status_code = response.status_code
+        response = response.json()
+        return response
+    else:
+        logger.error('Error status_code')
+        raise Exception(f'Error {status_code}')
 
 
 def check_response(response):
-
-    ...
+    """Verification the API for correctness."""
+    if not isinstance(response, dict):
+        logger.error('API is not dict')
+        raise TypeError('API is not dict')
+    try:
+        response_hw = response['homeworks']
+        if isinstance(response_hw, list) is False:
+            logger.error('Homeworks are not list')
+            raise TypeError('Homeworks are not list')
+    except Exception:
+        logger.info("No key 'homeworks'")
+        raise KeyError("No key 'homeworks'")
+    try:
+        homework = response_hw[0]
+    except Exception:
+        logger.info('Reviewer did not start the check')
+        raise IndexError('Reviewer did not start the check')
+    return homework
 
 
 def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
+    """
+    Status homework.
 
-    ...
-
-    verdict = ...
-
-    ...
-
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    Extracting the status of this work from information
+    about a particular homework.
+    """
+    if 'homework_name' not in homework:
+        logger.error('This key is not valid')
+        raise KeyError('This key is not valid')
+    if 'status' not in homework:
+        logger.error('This key is not valid')
+        raise Exception('This key is not valid')
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
+    if homework_status in HOMEWORK_STATUSES:
+        verdict = HOMEWORK_STATUSES[homework_status]
+        logger.info('Check status changed')
+        tex = f"Изменился статус проверки работы \"{homework_name}\".{verdict}"
+        return tex
 
 
 def check_tokens():
-    ...
+    """Verification environment variable."""
+    if (TELEGRAM_TOKEN is None and
+        PRACTICUM_TOKEN is None and
+            TELEGRAM_CHAT_ID is None):
+        logger.critical('Environment variables not found')
+        return False
+    else:
+        return True
 
 
 def main():
-    """Основная логика работы бота."""
-
-    ...
-
+    """General logic of work."""
+    if not check_tokens():
+        logger.critical('Environment variable not found')
+        raise Exception('Environment variable not found')
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-
-    ...
-
+    status = ''
     while True:
         try:
-            response = ...
-
-            ...
-
-            current_timestamp = ...
+            response = get_api_answer(current_timestamp)
+            homework = check_response(response)
+            homework_status = parse_status(homework)
+            if homework_status != homework.get(status):
+                message = parse_status(homework)
+                send_message(bot, message)
+            logger.info(
+                'Update not found, please check after 10 minutes'
+            )
             time.sleep(RETRY_TIME)
 
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
-            time.sleep(RETRY_TIME)
-        else:
-            ...
+            message = f'{error}'
+            logger.error(message)
+            send_message(bot, message)
+        time.sleep(RETRY_TIME)
 
 
 if __name__ == '__main__':
