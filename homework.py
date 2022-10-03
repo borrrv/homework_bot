36b46a7ee69f2c28542aa5ade97217b111dbb429
Владислав Бородай <vrboroday@yandex.ru>
2022-10-03 20:13:26 +0400

    Corrected

--- homework.py
+++ homework.py
@@ -8,6 +8,7 @@ from http import HTTPStatus
 from logging import FileHandler, Formatter, getLogger, StreamHandler
 from dotenv import load_dotenv
 
+import settings
 import os
 import telegram
 import requests
@@ -22,16 +23,9 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_TIME = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
+
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
-
-HOMEWORK_STATUSES = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.'
-}
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -51,6 +45,7 @@ file.setFormatter(formatter)
 
 def send_message(bot, message):
     """Message sent from bot."""
+    logger.info('Start message sent')
     try:
         bot.send_message(chat_id=TELEGRAM_CHAT_ID,
                          text=message)
@@ -63,36 +58,34 @@ def get_api_answer(current_timestamp):
     """Request to API."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-    response = requests.get(url=ENDPOINT,
-                            headers=HEADERS,
-                            params=params)
-    logger.info('Request to API')
+    try:
+        response = requests.get(url=settings.ENDPOINT,
+                                headers=HEADERS,
+                                params=params)
+        logger.info('Request to API')
+    except Exception:
+        logger.error('Network problem, please try again later')
     if response.status_code == HTTPStatus.OK:
         status_code = response.status_code
         response = response.json()
         return response
     else:
-        logger.error('Error status_code')
         raise Exception(f'Error {status_code}')
 
 
 def check_response(response):
     """Verification the API for correctness."""
     if not isinstance(response, dict):
-        logger.error('API is not dict')
         raise TypeError('API is not dict')
     try:
         response_hw = response['homeworks']
         if isinstance(response_hw, list) is False:
-            logger.error('Homeworks are not list')
             raise TypeError('Homeworks are not list')
     except Exception:
-        logger.info("No key 'homeworks'")
         raise KeyError("No key 'homeworks'")
     try:
         homework = response_hw[0]
     except Exception:
-        logger.info('Reviewer did not start the check')
         raise IndexError('Reviewer did not start the check')
     return homework
 
@@ -105,15 +98,13 @@ def parse_status(homework):
     about a particular homework.
     """
     if 'homework_name' not in homework:
-        logger.error('This key is not valid')
         raise KeyError('This key is not valid')
     if 'status' not in homework:
-        logger.error('This key is not valid')
         raise Exception('This key is not valid')
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
-    if homework_status in HOMEWORK_STATUSES:
-        verdict = HOMEWORK_STATUSES[homework_status]
+    if homework_status in settings.HOMEWORK_STATUSES:
+        verdict = settings.HOMEWORK_STATUSES[homework_status]
         logger.info('Check status changed')
         tex = f"Изменился статус проверки работы \"{homework_name}\".{verdict}"
         return tex
@@ -133,7 +124,6 @@ def check_tokens():
 def main():
     """General logic of work."""
     if not check_tokens():
-        logger.critical('Environment variable not found')
         raise Exception('Environment variable not found')
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
@@ -149,13 +139,13 @@ def main():
             logger.info(
                 'Update not found, please check after 10 minutes'
             )
-            time.sleep(RETRY_TIME)
+            time.sleep(settings.RETRY_TIME)
 
         except Exception as error:
             message = f'{error}'
             logger.error(message)
             send_message(bot, message)
-        time.sleep(RETRY_TIME)
+        time.sleep(settings.RETRY_TIME)
 
 
 if __name__ == '__main__':
